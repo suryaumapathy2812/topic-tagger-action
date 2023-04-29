7aaefc393462c04bafe54a4fa7fe056c2a8c5f32
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-04-30 03:10:51 +0530

    chore: added +/- emoji

--- src/comment.js
+++ src/comment.js
@@ -11,7 +11,7 @@ const formatTable = (oldTopicOutput, newTopicOutput) => {
             core.info("new count =>>>>>>>>>> " + newCount)
             core.info("old count =>>>>>>>>>> " + oldCount)
             const diffCount = newCount - oldCount;
-            const diffSign = diffCount > 0 ? `+ ` : (diffCount < 0 ? `- ` : `0`);
+            const diffSign = diffCount > 0 ? `:heavy_plus_sign: ` : (diffCount < 0 ? `:heavy_minus_sign: ` : ``);
 
             core.info(`diffcount =>>>>>>>>>> ${diffCount}`)
 
