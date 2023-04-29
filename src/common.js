8d076738f98e851567193e95f5e749fa9be7a902
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-04-29 16:10:36 +0530

    refactor: v4 code broken into small snippets

---
+++ src/common.js
@@ -0,0 +1,142 @@
+const fs = require("fs")
+const core = require("@actions/core")
+const cheerio = require("cheerio");
+// const esprima = require('esprima');
+const { default: axios } = require("axios");
+
+// function isOverlapping(range1, range2) {
+//     return range1[0] < range2[1] && range1[1] > range2[0];
+// }
+
+
+// function isExtracted(range, extractedRanges) {
+//     for (const extractedRange of extractedRanges) {
+//         if (isOverlapping(range, extractedRange)) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// function extractCodeChunks(code) {
+
+//     let parsedCode;
+
+//     try {
+//         parsedCode = esprima.parseScript(code, { range: true, tokens: true, comment: true });
+//     } catch (error) {
+//         return false
+//     }
+
+//     const chunks = [];
+//     const extractedRanges = [];
+
+
+//     parsedCode.body.forEach((node) => {
+//         if (!isExtracted(node.range), extractedRanges) {
+//             const chunk = code.slice(node.range[0], node.range[1]);
+//             chunks.push(chunk);
+//             extractedRanges.push(node.range);
+//         }
+//     });
+
+//     return chunks;
+// }
+
+
+function readCodebase(directory) {
+    const entries = fs.readdirSync(directory, { withFileTypes: true })
+        .filter(({ name: file }) => {
+
+            if (
+                file.startsWith('.') ||
+                file === 'node_modules' ||
+                file === 'dist' ||
+                file.includes(".config") ||
+                file.includes(".bundle.js") ||
+                // file.includes("bootstrap.js") ||
+                file.includes(".esm.js") ||
+                file.includes(".cjs.js") ||
+                file.includes(".min.js") ||
+                file.includes(".test.js")
+            ) {
+                return false;
+            } else {
+                return true
+            }
+
+        });
+
+    const filePaths = entries
+        .map(({ name: file }) => {
+            const filePath = directory + '/' + file;
+            const stats = fs.statSync(filePath);
+
+            if (stats.isDirectory()) return readCodebase(filePath)
+
+            if (stats.isFile() && (file.endsWith('.js') || file.endsWith(".html"))) {
+                return { path: filePath }
+            }
+        })
+        .filter(entry => entry);
+
+    return filePaths.flat();
+}
+
+function extractJsFromHtml(filePath) {
+    const htmlContent = fs.readFileSync(filePath.path, 'utf8');
+    const $ = cheerio.load(htmlContent);
+    const scriptTags = $('script');
+
+    const scripts = [];
+    scriptTags.each((_, scriptTag) => {
+        const scriptContent = $(scriptTag).html();
+        if (scriptContent) {
+            scripts.push(scriptContent);
+        }
+    });
+
+    return scripts;
+}
+
+function extractJsCode(file) {
+
+    core.info(file);
+
+    let code;
+
+    if ((file.path).endsWith('.html')) {
+        code = extractJsFromHtml(file).join(" \n\n ");
+        // console.debug(`JavaScript code in ${file}:\n`, JSON.stringify(code, null, 2));
+    } else if ((file.path).endsWith('.js')) {
+        const _code = fs.readFileSync(file.path, 'utf8');
+        code = _code;
+    }
+
+    return code;
+
+}
+
+
+async function tagTopics(code) {
+    try {
+        const { concepts } = (
+            await axios.post("https://core.api.learn2build.in/api/v4/javascript", {
+                body: {
+                    "sourceLanguage": "JavaScript",
+                    "targetLanguage": "JavaScript",
+                    "code": code
+                },
+                headers: { 'Content-Type': 'application/json' }
+            })).data
+
+        return (await concepts)
+    } catch (error) {
+        core.error(error.message)
+        throw new Error(error.message, error.status)
+    }
+}
+
+
+module.exports = { readCodebase, extractJsCode, extractJsFromHtml, tagTopics }
--- src/v4.js
+++ src/v4.js
@@ -1,45 +1,6 @@
 const core = require("@actions/core")
-const esprima = require('esprima'); // If using Node.js and npm
-const fs = require('fs');
-const cheerio = require("cheerio");
-const { default: axios } = require('axios');
+const { readCodebase, extractJsCode, tagTopics } = require("./common.js")
 
-function extractCodeChunks(code) {
-
-    let parsedCode;
-
-    try {
-        parsedCode = esprima.parseScript(code, { range: true, tokens: true, comment: true });
-    } catch (error) {
-        return false
-    }
-
-    const chunks = [];
-    const extractedRanges = [];
-
-    function isOverlapping(range1, range2) {
-        return range1[0] < range2[1] && range1[1] > range2[0];
-    }
-
-    function isExtracted(range) {
-        for (const extractedRange of extractedRanges) {
-            if (isOverlapping(range, extractedRange)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    parsedCode.body.forEach((node) => {
-        if (!isExtracted(node.range)) {
-            const chunk = code.slice(node.range[0], node.range[1]);
-            chunks.push(chunk);
-            extractedRanges.push(node.range);
-        }
-    });
-
-    return chunks;
-}
 
 function mergeDeep(target, ...sources) {
     if (!sources.length) return target;
@@ -67,127 +28,30 @@ function isObject(item) {
     return item && typeof item === 'object' && !Array.isArray(item);
 }
 
-function readCodebase(directory) {
-    const entries = fs.readdirSync(directory, { withFileTypes: true })
-        .filter(({ name: file }) => {
-
-            if (
-                file.startsWith('.') ||
-                file === 'node_modules' ||
-                file === 'dist' ||
-                file.includes(".config") ||
-                file.includes(".bundle.js") ||
-                // file.includes("bootstrap.js") ||
-                file.includes(".esm.js") ||
-                file.includes(".cjs.js") ||
-                file.includes(".min.js") ||
-                file.includes(".test.js")
-            ) {
-                return false;
-            } else {
-                return true
-            }
-
-        });
-
-    const filePaths = entries
-        .map(({ name: file }) => {
-            const filePath = directory + '/' + file;
-            const stats = fs.statSync(filePath);
-
-            if (stats.isDirectory()) return readCodebase(filePath)
-
-            if (stats.isFile() && (file.endsWith('.js') || file.endsWith(".html"))) {
-                return { path: filePath }
-            }
-        })
-        .filter(entry => entry);
-
-    return filePaths.flat();
-}
-
-
-function extractJsFromHtml(filePath) {
-    const htmlContent = fs.readFileSync(filePath.path, 'utf8');
-    const $ = cheerio.load(htmlContent);
-    const scriptTags = $('script');
-
-    const scripts = [];
-    scriptTags.each((_, scriptTag) => {
-        const scriptContent = $(scriptTag).html();
-        if (scriptContent) {
-            scripts.push(scriptContent);
-        }
-    });
-
-    return scripts;
-}
-
 const executionScript = async (directory) => {
     try {
 
         core.info("Using V4 of topic tagging");
 
         const filePaths = readCodebase(directory).flat();
-
         console.info("** File Paths are \n")
         console.info(filePaths.map(file => JSON.stringify(file)))
 
-        let index = 0;
+
+        const codeChunks = filePaths.map(file => {
+            return extractJsCode(file)
+        })
+        console.info("**No Of extracted JS Code: \n")
+        console.info(codeChunks.length)
+
 
         const implementations = []
 
-        for (const file of filePaths) {
-
-            core.info(file)
-            let code = ''
-
-            if ((file.path).endsWith('.html')) {
-                code = extractJsFromHtml(file).join(" \n\n ");
-                // console.debug(`JavaScript code in ${file}:\n`, JSON.stringify(code, null, 2));
-            } else if ((file.path).endsWith('.js')) {
-                const _code = fs.readFileSync(file.path, 'utf8');
-                code = _code;
-            }
-
-            const codeChunks = extractCodeChunks(code);
-
-            if (codeChunks === false) {
-                console.debug(`Skipping file due to error`);
-                index++
-                return
-            }
-
-            const response = []
-            for (const chunk of codeChunks) {
-                const result = { chunk, concepts: {} };
-                const { concepts } = (
-                    await axios.post("https://core.api.learn2build.in/api/v4/javascript", {
-                        body: {
-                            "sourceLanguage": "JavaScript",
-                            "targetLanguage": "JavaScript",
-                            "code": chunk
-                        },
-                        headers: { 'Content-Type': 'application/json' }
-                    })).data
-                result.concepts = concepts
-                response.push(result);
-            }
-
-            const analysisResults = await Promise
-                .all(response);
-
-            core.info(JSON.stringify(analysisResults, null, 2))
-
-            const temporary = analysisResults
-                .reduce((accumulator, currentObject) => mergeDeep(accumulator, currentObject), {});
-
-            core.info(temporary)
-
-            const finalResult = temporary.concepts;
-            filePaths[index]["topics"] = finalResult
-            index++
-            implementations.push(finalResult);
+        for (const code of codeChunks) {
+            const concepts = tagTopics(code);
+            core.info(JSON.stringify(concepts, null, 2))
+            // code.concepts = concepts
+            implementations.push(concepts)
         }
 
         const consolidatedData = implementations
