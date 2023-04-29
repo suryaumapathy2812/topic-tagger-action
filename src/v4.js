ef7a3cd7ff0d15a7a6e026214f44f0a14d3f9684
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-04-29 12:19:46 +0530

    feat: moved core logic to api

--- src/v4.js
+++ src/v4.js
@@ -1,8 +1,7 @@
 const esprima = require('esprima'); // If using Node.js and npm
 const fs = require('fs');
-const parser = require('@babel/parser');
-const traverse = require('@babel/traverse').default;
 const cheerio = require("cheerio");
+const { default: fetch } = require('node-fetch');
 
 function extractCodeChunks(code) {
 
@@ -40,386 +39,6 @@ function extractCodeChunks(code) {
 
     return chunks;
 }
-function generateAST(code) {
-    try {
-        const ast = parser.parse(code, {
-            sourceType: 'module',
-            plugins: [],
-        });
-
-        return ast;
-    } catch (error) {
-        console.error('Error generating AST:', error);
-        return null;
-    }
-}
-
-function analyzeAST(ast, conceptPatterns) {
-    const conceptsFound = {};
-
-    traverse(ast, {
-        enter(path) {
-            for (const topic in conceptPatterns) {
-                if (!conceptsFound[topic]) {
-                    conceptsFound[topic] = {};
-                }
-
-                const concepts = conceptPatterns[topic];
-
-                for (const concept in concepts) {
-                    const patterns = concepts[concept];
-                    for (const pattern of patterns) {
-                        if (pattern(path)) {
-                            if (!conceptsFound[topic][concept]) {
-                                conceptsFound[topic][concept] = 0;
-                            }
-                            conceptsFound[topic][concept]++;
-                            break;
-                        }
-                    }
-                }
-            }
-        },
-    });
-
-    return conceptsFound;
-}
-
-const conceptPatterns = {
-    Variables: {
-        Variables: [
-            (path) => path.isVariableDeclaration(),
-        ],
-    },
-    Array: {
-        'Array Declarations': [
-            (path) => (
-                path.isVariableDeclarator()
-                && path.get('init').isArrayExpression()
-            ),
-        ],
-        'Array Methods': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['map', 'filter', 'reduce', 'forEach'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-    },
-    Object: {
-        'Object Declarations': [
-            (path) => (
-                path.isVariableDeclarator()
-                && path.get('init').isObjectExpression()
-            ),
-        ],
-        'Object Methods': [
-            (path) => (
-                path.isObjectMethod() || (
-                    path.isObjectProperty()
-                    && (path.get('value').isFunctionExpression() || path.get('value').isArrowFunctionExpression())
-                )
-            ),
-        ],
-    },
-    Sets: {
-        'Sets Declarations': [
-            (path) => (
-                path.isVariableDeclarator()
-                && path.get('init').isCallExpression()
-                && path.get('init.callee').isIdentifier({ name: 'Set' })
-            ),
-        ],
-    },
-    Maps: {
-        'Maps Declarations': [
-            (path) => (
-                path.isVariableDeclarator()
-                && path.get('init').isCallExpression()
-                && path.get('init.callee').isIdentifier({ name: 'Map' })
-            ),
-        ],
-    },
-    Loops: {
-        'For Loops': [
-            (path) => path.isForStatement(),
-        ],
-        'While Loops': [
-            (path) => path.isWhileStatement(),
-        ],
-        'Do-While Loops': [
-            (path) => path.isDoWhileStatement(),
-        ],
-        'For-Of Loops': [
-            (path) => path.isForOfStatement(),
-        ],
-        'For-In Loops': [
-            (path) => path.isForInStatement(),
-        ],
-        'Nested Loops': [
-            (path) => (
-                path.isForStatement() || path.isWhileStatement() || path.isDoWhileStatement()
-            ) && (
-                    path.findParent((parentPath) => (
-                        // eslint-disable-next-line max-len
-                        parentPath.isForStatement() || parentPath.isWhileStatement() || parentPath.isDoWhileStatement()
-                    ))
-                ),
-        ],
-
-    },
-    Functions: {
-        'Function Declarations': [
-            (path) => path.isFunctionDeclaration(),
-        ],
-        'Arrow Functions': [
-            (path) => path.isArrowFunctionExpression(),
-        ],
-        'IIFE Functions': [
-            (path) => (
-                path.isCallExpression()
-                && (path.get('callee').isFunctionExpression() || path.get('callee').isArrowFunctionExpression())
-            ),
-        ],
-        'Nameless Functions': [
-            (path) => path.isFunctionExpression(),
-        ],
-    },
-    Operators: {
-        'Arithmetic Operators': [
-            (path) => (
-                path.isBinaryExpression()
-                && ['+', '-', '*', '/', '%', '**'].includes(path.node.operator)
-            ),
-        ],
-        'Comparison Operators': [
-            (path) => (
-                path.isBinaryExpression()
-                && ['==', '===', '!=', '!==', '>', '>=', '<', '<='].includes(path.node.operator)
-            ),
-        ],
-        'Logical Operators': [
-            (path) => (
-                path.isLogicalExpression()
-                && ['&&', '||', '??'].includes(path.node.operator)
-            ),
-        ],
-        'Unary Operators': [
-            (path) => (
-                path.isUnaryExpression()
-                && ['+', '-', '!', '~', 'typeof', 'void', 'delete'].includes(path.node.operator)
-            ),
-        ],
-        'Ternary Operator': [
-            (path) => path.isConditionalExpression(),
-        ],
-    },
-    'Error Handling': {
-        'Try Statements': [
-            (path) => path.isTryStatement(),
-        ],
-        'Catch Statements': [
-            (path) => path.isCatchClause(),
-        ],
-        'Throw Statements': [
-            (path) => path.isThrowStatement(),
-        ],
-    },
-    Classes: {
-        'Class Declarations': [
-            (path) => path.isClassDeclaration(),
-        ],
-        'Class Expressions': [
-            (path) => path.isClassExpression(),
-        ],
-        'Constructor Methods': [
-            (path) => (
-                path.isClassMethod()
-                && path.node.kind === 'constructor'
-            ),
-        ],
-        'Instance Methods': [
-            (path) => (
-                path.isClassMethod()
-                && path.node.kind !== 'constructor'
-                && !path.node.static
-            ),
-        ],
-        'Static Methods': [
-            (path) => (
-                path.isClassMethod()
-                && path.node.kind !== 'constructor'
-                && path.node.static
-            ),
-        ],
-    },
-    Conditionals: {
-        'If Statements': [
-            (path) => path.isIfStatement(),
-        ],
-        'Else Statements': [
-            (path) => path.isIfStatement() && path.node.alternate && path.node.alternate.type === 'BlockStatement',
-        ],
-        'Else If Statements': [
-            (path) => path.isIfStatement() && path.node.alternate && path.node.alternate.type === 'IfStatement',
-        ],
-        'Switch Statements': [
-            (path) => path.isSwitchStatement(),
-        ],
-    },
-    'Timing Events': {
-        setTimeout: [
-            (path) => path.isCallExpression() && path.get('callee').isIdentifier({ name: 'setTimeout' }),
-        ],
-        setInterval: [
-            (path) => path.isCallExpression() && path.get('callee').isIdentifier({ name: 'setInterval' }),
-        ],
-    },
-    ES5: {
-        'Spread Operator': [
-            (path) => path.isSpreadElement(),
-        ],
-        'Destructuring Assignment': [
-            (path) => path.isObjectPattern() || path.isArrayPattern(),
-        ],
-    },
-    AJAX: {
-        Async: [
-            (path) => (path.isFunction() && path.node.async),
-        ],
-        Promise: [
-            (path) => path.isNewExpression() && path.get('callee').isIdentifier({ name: 'Promise' }),
-        ],
-        Await: [
-            (path) => path.isCallExpression() && path.get('callee').isIdentifier({ name: 'await' }),
-        ],
-        Fetch: [
-            (path) => path.isCallExpression() && path.get('callee').isIdentifier({ name: 'fetch' }),
-        ],
-    },
-    DOM: {
-        'Event Listeners': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && path.get('callee.property').isIdentifier({ name: 'addEventListener' })
-            ),
-        ],
-        querySelector: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && path.get('callee.property').isIdentifier({ name: 'querySelector' })
-            ),
-        ],
-        createElement: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && path.get('callee.property').isIdentifier({ name: 'createElement' })
-            ),
-        ],
-        appendChild: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && path.get('callee.property').isIdentifier({ name: 'appendChild' })
-            ),
-        ],
-        prependChild: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && path.get('callee.property').isIdentifier({ name: 'prependChild' })
-            ),
-        ],
-    },
-    Forms: {
-        'Form Controls': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['reset', 'submit'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-        'Form Elements': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['elements'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-        'Form Validation': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['checkValidity', 'reportValidity', 'setCustomValidity'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-    },
-    Events: {
-        'Click Events': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['click', 'dblclick'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-        'Change Events': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['change', 'input', 'select'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-        'Keyboard Events': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['keydown', 'keyup', 'keypress'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-        'Mouse Events': [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['mousedown', 'mouseup', 'mousemove', 'mouseover', 'mouseout', 'mouseenter', 'mouseleave'].includes(path.get('callee.property').node.name)
-            ),
-        ],
-    },
-    'Web API': {
-        localStorage: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['setItem', 'getItem', 'removeItem', 'clear'].includes(path.get('callee.property').node.name)
-                && path.get('callee.object').isIdentifier({ name: 'localStorage' })
-            ),
-        ],
-        sessionStorage: [
-            (path) => (
-                path.isCallExpression()
-                && path.get('callee').isMemberExpression()
-                && ['setItem', 'getItem', 'removeItem', 'clear'].includes(path.get('callee.property').node.name)
-                && path.get('callee.object').isIdentifier({ name: 'sessionStorage' })
-            ),
-        ],
-        cookies: [
-            (path) => (
-                path.isAssignmentExpression()
-                && path.get('left').isMemberExpression()
-                && path.get('left.object').isIdentifier({ name: 'document' })
-                && path.get('left.property').isIdentifier({ name: 'cookie' })
-            ),
-        ],
-        'URL Params': [
-            (path) => (
-                path.isNewExpression()
-                && path.get('callee').isIdentifier({ name: 'URLSearchParams' })
-            ),
-        ],
-    },
-};
 
 function mergeDeep(target, ...sources) {
     if (!sources.length) return target;
@@ -503,8 +122,7 @@ function extractJsFromHtml(filePath) {
     return scripts;
 }
 
-
-const executionScript = (directory) => {
+const executionScript = async (directory) => {
     try {
 
         console.log("Using V4 of topic tagging");
@@ -519,7 +137,7 @@ const executionScript = (directory) => {
         const implementations = []
 
         filePaths
-            .forEach(file => {
+            .forEach(async file => {
 
                 console.log(file)
                 let code = ''
@@ -541,11 +159,41 @@ const executionScript = (directory) => {
                 }
 
 
-                const analysisResults = codeChunks.map((chunk) => {
-                    const ast = generateAST(chunk);
-                    const concepts = ast ? analyzeAST(ast, conceptPatterns) : [];
-                    return { chunk, concepts };
-                });
+
+                // for (const chunk of codeChunks) {
+                //     const concept = await fetch("https://core.api.learn2build.in/api/v4/javascript", {
+                //         method: "POST",
+                //         body: {
+                //             "sourceLanguage": "JavaScript",
+                //             "targetLanguage": "JavaScript",
+                //             "code": chunk
+                //         }
+                //     })
+
+                //     return { chunk, concept }
+                // }
+
+                const analysisResults = await Promise
+                    .all(
+                        codeChunks.map(async (chunk) => {
+                            const concepts = await fetch("https://core.api.learn2build.in/api/v4/javascript", {
+                                method: "POST",
+                                body: {
+                                    "sourceLanguage": "JavaScript",
+                                    "targetLanguage": "JavaScript",
+                                    "code": chunk
+                                }
+                            })
+                            return { chunk, concepts };
+                        })
+                    );
+
+
+                // const analysisResults = codeChunks.map((chunk) => {
+                //     const ast = generateAST(chunk);
+                //     const concepts = ast ? analyzeAST(ast, conceptPatterns) : [];
+                //     return { chunk, concepts };
+                // });
 
                 const temporary = analysisResults
                     .reduce((accumulator, currentObject) => mergeDeep(accumulator, currentObject), {});
